622a110bd4b82f66795a22ae4fca794c649a0b96
AprilKung <Phich2549@gmail.com>
2026-09-21 22:19:31 +0700

    Cell02 Ex00

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,10 @@
+try:
+    number = float(input())
+    
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+
+except ValueError:
+    print("Number Pleae.")
