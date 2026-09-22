4a6d2ff4d61ea8df6b2b9ae75ab7249e3917eb10
AprilKung <Phich2549@gmail.com>
2026-09-22 14:13:32 +0700

    cell04 update

---
+++ Cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+
+print("*********************************************") 
+    
+print(f"{num1} + {num2} = {int(num1 + num2)}")
+print(f"{num1} - {num2} = {int(num1 - num2)}")
+print(f"{num1} / {num2} = {int(num1 / num2)}") 
+print(f"{num1} * {num2} = {int(num1 * num2)}")
+
+print("Thank you!") 
